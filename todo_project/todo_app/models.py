224baaf66d1d1f070586828488fccb34251ba20f
Moghany <m.osama.elmoghany@gmail.com>
2024-06-29 15:00:04 +0300

    added views & urls

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # model => variables
 
 # Create your models here.
-class Todo_view(models.Model):
+class Todo_model(models.Model):
 	task = models.CharField(max_length=200, blank=True, default='')
 	completed = models.BooleanField(default=False)
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views 	 import APIView
-from todo_app.models import Todo
+from todo_app.models import Todo_model
 
 # Create your views here.
 class TodoView(APIView):
 	def post(self, request):
 		data = request.data.dict()
-		todo = Todo(task=data['task'], completed=data['completed'])
+		todo = Todo_model(task=data['task'], completed=data['completed'])
 		todo.save()
 		return Response(200)
 		
 	def get(self, request):
-		items = [(x.task, x.completed) for x in Todo.objects.all()]
+		items = [(x.task, x.completed) for x in Todo_model.objects.all()]
 		return Response(items)
