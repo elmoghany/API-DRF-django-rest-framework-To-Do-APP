6e397343d73fbe2824651ec438e2735bac7f7af2
Moghany <m.osama.elmoghany@gmail.com>
2024-07-01 16:56:28 +0300

    terraform

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,31 @@
 variable REGION {
   default = "us-east-1"
 }
+variable AMI {
+  default = "ami-0c55b159cbfafe1f0"
+}
+variable "PROJECT" {
+  default = "todo-docker-terraform"
+}
+variable "PREFIX" {
+  default = "MOGHANY-"
+}
+variable "USER" {
+  default = "ubuntu"
+}
+variable "S3_BUCKET" {
+  default = "moghany-django"
+}
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+variable "KEY_NAME" {
+  description = "name of the ssh key pair"
+  default = "moghany-todo-key"
+}
+variable "PUB_KEY" {
+  default = "moghany-todo-keypair.pub"
+}
+variable "priv_KEY" {
+  default = "moghany-todo-keypair"
+}
